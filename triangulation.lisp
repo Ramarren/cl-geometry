d7b22abb8bb34cdac3bd304aa24fd0e9c9d408de
Ramarren <ramarren@gmail.com>
2007-10-06 11:38:00 +0200

    Remove recalculating edge list on every ear removal.

--- triangulation.lisp
+++ triangulation.lisp
@@ -57,15 +57,15 @@
 	  (between-p c d a)
 	  (between-p c d b))))
 
-(defun possible-diagonal-p (diag edge-list)
-  "Checks if diag does not intersect any edge in edge list."
-  (notany #'(lambda (e)
-	      (and (not (point-equal-p (start diag) (start e)))
-		   (not (point-equal-p (start diag) (end e)))
-		   (not (point-equal-p (end diag) (start e)))
-		   (not (point-equal-p (end diag) (end e)))
-		   (intersect-p (start diag)(end diag)(start e)(end e))))
-	  edge-list))
+(defun possible-diagonal-p (ring-diag)
+  "Checks if ring-diag does not intersect any edge in a ring."
+  (iterate (for node initially (start ring-diag) then (next-node node))
+	   (until (and (eql node (start ring-diag))(not (first-iteration-p))))
+	   (and (not (eql (start ring-diag) node))
+		(not (eql (end ring-diag) node))
+		(not (eql (start ring-diag) (next-node node)))
+		(not (eql (end ring-diag) (next-node node)))
+		(intersect-p (start ring-diag)(end ring-diag) node (next-node node)))))
 
 (defun in-cone-p (ring-node b)
   "Is line segment ring-node->b in cone defined by angle with vertex defined by ring-node?"
@@ -78,14 +78,13 @@
 	(not (and (left-on-p a b a+)
 		  (left-on-p b a a-))))))
 
-(defun diagonal-p (ring-node-a ring-node-b edge-list)
+(defun diagonal-p (ring-node-a ring-node-b)
   "Is a line segment between two nodes a diagonal of polygon with edges edge-list?"
   (and (in-cone-p ring-node-a (val ring-node-b))
        (in-cone-p ring-node-b (val ring-node-a))
        (possible-diagonal-p (make-instance 'line-segment
-					   :start (val ring-node-a)
-					   :end (val ring-node-b))
-			    edge-list)))
+					   :start ring-node-a
+					   :end ring-node-b))))
 
 ;;; ear removal method - O(2)
 
@@ -95,17 +94,15 @@
 
 (defun ear-init (polygon)
   "Takes a list of points and creates a ring initialized with ear data."
-  (let ((edge-list (edge-list-from-point-list polygon))
-	(ring-head (double-linked-ring-from-point-list polygon 'ear-ring-node)))
+  (let ((ring-head (double-linked-ring-from-point-list polygon 'ear-ring-node)))
     (iterate (for node initially ring-head then (next-node node))
 	     (until (and (eq node ring-head)
 			 (not (first-iteration-p))))
 	     (setf (ear node) (diagonal-p (prev-node node)
-					  (next-node node)
-					  edge-list)))
-    (values ring-head edge-list)))
+					  (next-node node))))
+    ring-head))
 
-(defun remove-ear (node edge-list)
+(defun remove-ear (node)
   "Remove an ear centered on node from ring, returning new node, the removed ear and new edge list."
   (let ((v2 node))
     (let ((v1 (prev-node v2))
@@ -113,32 +110,24 @@
       (let ((v0 (prev-node v1))
 	    (v4 (next-node v3)))
 	(let ((ear (list (val v2)(val v1)(val v3))))
-	  (let ((new-edge-list (cons (make-instance 'line-segment
-						    :start (val v1)
-						    :end (val v3))
-				     (remove-if #'(lambda (edge)
-						    (or (point-equal-p (val v2) (start edge))
-							(point-equal-p (val v2) (end edge))))
-						edge-list))))
-	    (setf (next-node v1) v3
-		  (prev-node v3) v1
-		  (ear v1) (diagonal-p v0 v3 new-edge-list)
-		  (ear v3) (diagonal-p v1 v4 new-edge-list))
-	    (values v3 ear new-edge-list)))))))
+	  (setf (next-node v1) v3
+		(prev-node v3) v1
+		(ear v1) (diagonal-p v0 v3)
+		(ear v3) (diagonal-p v1 v4))
+	  (values v3 ear))))))
 
 (defun triangulate (polygon)
   "Triangulate polygon. Returns list of triangles."
   (let ((num-vertices (length polygon))
 	(ear-list))
-    (multiple-value-bind (ring-head edge-list) (ear-init polygon)
+    (let ((ring-head (ear-init polygon)))
       (iterate (while (> num-vertices 3))
 	       (with node = ring-head)
 	       (if (ear node)
-		   (multiple-value-bind (new-node ear new-edge-list) (remove-ear node edge-list)
-		       (setf node new-node
-			     edge-list new-edge-list)
-		       (decf num-vertices)
-		       (push ear ear-list))
+		   (multiple-value-bind (new-node ear) (remove-ear node)
+		     (setf node new-node)
+		     (decf num-vertices)
+		     (push ear ear-list))
 		   (setf node (next-node node)))
 	       (finally (push (point-list-from-ring node) ear-list))))
     ear-list))
